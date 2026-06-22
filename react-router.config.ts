496a3d93f4f7c60223b12dafcfc2606d3420b184
DannyBase2025 <dannybase2025@gmail.com>
2026-06-22 14:24:10 +0800

    fixing gh pages

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  basename: "/your-repo-name",
+  basename: "/Basic-SocMed",
 } satisfies Config;
