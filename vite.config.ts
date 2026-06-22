1b5c83f9eeb4983e516aaa45e858cf1a651b2efc
DannyBase2025 <dannybase2025@gmail.com>
2026-06-22 14:21:57 +0800

    fixing gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Basic-SocMed/",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
